0cb30c9b09ba7fee64412e7236e5c536c9d9ccf4
Natnael-Tilahun <natnaeltilahun97gmail.com>
2021-12-18 10:02:01 +0300

    test header layout

--- pages/components/header.js
+++ pages/components/header.js
@@ -38,7 +38,7 @@ const Header = () => {
         className="text-light border border-light"
       />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className=" ms-auto mx-5">
+        {/* <Nav className=" ms-auto mx-5">
           <Nav.Link href="/" className=" ">
             <Link href="/" passHref className="">
               <a className="text-light px-2 text-decoration-none ">Home</a>
@@ -63,6 +63,52 @@ const Header = () => {
               </a>
             </Link>
           </Nav.Link>
+        </Nav> */}
+
+        <Nav className="me-lg-5 align-middle" className={styles.navMenu}>
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-start justify-content-lg-start">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <Nav.Link href="/" className="text.white ">
+                    <Link href="/" passHref className="text-white ">
+                      <a className=" text-white px-2 text-decoration-none ">
+                        <span className="fa fa-home fa-lg"></span> Home
+                      </a>
+                    </Link>
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link href="/about">
+                    <Link href="/about" passHref>
+                      <a className="text-white px-2 text-decoration-none align-middle">
+                        <span className="fa fa-info fa-lg"></span> About
+                      </a>
+                    </Link>
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link href="/contact">
+                    <Link href="/contact" passHref>
+                      <a className="text-white px-2 text-decoration-none ">
+                        <span className="fa fa-address-card fa-lg"></span>{' '}
+                        Contact
+                      </a>
+                    </Link>
+                  </Nav.Link>
+                </li>
+                <li>
+                  <Nav.Link href="/steps">
+                    <Link href="/steps" passHref>
+                      <a className="text-white px-2  text-decoration-none align-middle">
+                        <span className="fa fa-info-square fa-lg"></span>Steps
+                      </a>
+                    </Link>
+                  </Nav.Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </Nav>
       </Navbar.Collapse>
       {/* </Container> */}
