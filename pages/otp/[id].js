011e562a41979f6de18b45631c4519459b18cad2
Natnael-Tilahun <natnaeltilahun97gmail.com>
2021-12-20 12:06:02 +0300

    post data to api

--- pages/otp/[id].js
+++ pages/otp/[id].js
@@ -105,7 +105,7 @@ const Confirmation = ({ ninja }) => {
             }}
             required
           />
-          <div className=""> error message fssdfsa</div>
+          <div className="invalid-feedback"> error message fssdfsa</div>
         </div>
 
         <div className="col-12">
